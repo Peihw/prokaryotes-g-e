83f6a231058fc96212e04a4dde4bd4f34933f52b
Peihw <92356350+Peihw@users.noreply.github.com>
2024-02-16 21:49:40 +0800

    Delete FP directory

--- FP/codes/estimate.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef ESTIMATE_H
-#define ESTIMATE_H
-
-#include "common.h"
-
-Str estimate(Str Out, Str GenoID, rec red);
-
-#endif
--- FP/codes/rps.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef RPS_H
-#define RPS_H
-
-#include "common.h"
-
-Str rps(Str Out, Str GenoID, rec red, Ma_Str_X tax, Ma_Str_Str cog);
-
-#endif
